b215678c3b63e0dcc4be625cfaa549c8c54a7544
sseukjin <69473171+sseukjin@users.noreply.github.com>
2020-08-13 17:48:13 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {Button, Progress} from 'semantic-ui-react'
+import {Button, Input, Progress} from 'semantic-ui-react'
+
+function Login2(){
+
+    return(
+      <div>
+      <Input/>
+      <Button onClick = { (e) => {
+        console.log(e)
+        alert(e.target.value)
+      } } > 로그인 </Button>
+      </div>
+
+    )
+}
+
+class Login extends React.Component{
+  constructor(){
+      super()
+      this.state = {id : "환영합니다!"}
+  }
+
+  render(){
+    return(
+      <div>
+      <Input onChange = { (e) => this.setState({id : e.target.value})}/>
+      <Button onClick = { (e) => {
+        console.log(e)
+        alert(this.state.id)
+      } } > Login </Button>
+      </div>
+
+    )
+
+  constructor()
+  }
+}
 
 function App() {
   return (
     <div>
     <p>이소진</p>
-    <Button Primary> hi </Button>
+    <Button> hi </Button>
+    <Login / >
     <Progress percent = {33} indicating/>
     </div>
   );
