fd23be13b92e0c61ab99dcc8c9e8b6cc53ddea77
sseukjin <69473171+sseukjin@users.noreply.github.com>
2020-08-13 12:06:37 +0900

    bb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
+    <div>
     <p>이소진</p>
+    <Button Primary> hi </Button>
+    <Progress percent = {33} indicating/>
+    </div>
   );
 }
 
